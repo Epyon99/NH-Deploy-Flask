3bf9b8477f7a30409346b4e7a13421131a38589f
Moises Chirinos <mfrchg@hotmail.com>
2024-11-24 11:35:21 -0500

    flask run

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask,jsonify,request # request es libreria de flask para procesar los request del cliente
 
 app = Flask(__name__)
-api = Api(app)
 
-item_fields={
-    'id':fields.Integer,
-    'name':fields.String,
-    'price':fields.Float
-}
 item = [
     {'id':1,'name':'Item1','price':100},
     {'id':2,'name':'Item2','price':200}
